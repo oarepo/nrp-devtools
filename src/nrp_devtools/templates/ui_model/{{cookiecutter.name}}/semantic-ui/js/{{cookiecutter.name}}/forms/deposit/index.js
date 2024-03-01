6e93369a3290e598629faca24de423d09877fc18
Miroslav Bauer <mirekys@gmail.com>
2024-03-01 19:09:12 +0100

    refactor: update search & form app ui with fresh oarepo-ui utils

--- src/nrp_devtools/templates/ui_model/{{cookiecutter.name}}/semantic-ui/js/{{cookiecutter.name}}/forms/deposit/index.js
+++
@@ -1,8 +0,0 @@
-import { createFormAppInit } from "@js/oarepo_ui";
-import { DepositForm } from "./DepositForm"
-
-export const overriddenComponents = {
-    "FormApp.layout": DepositForm,
-};
-
-createFormAppInit(overriddenComponents);
