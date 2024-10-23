1d0d0c23e6fe335fd8f50ad5bf5a8f2f593dbe9f
Alzbeta Pokorna <alzbetapokorna@eduroam-tlh223.cesnet.cz>
2024-10-23 14:40:04 +0200

    start proxy command

--- src/nrp_devtools/cli/__init__.py
+++ src/nrp_devtools/cli/__init__.py
@@ -12,6 +12,7 @@ from .translations import translations_command
 from .ui import ui_group
 from .upgrade import upgrade_command
 from .watch import watch_command
+from .proxy import start_proxy
 
 __all__ = [
     "nrp_command",
@@ -28,4 +29,5 @@ __all__ = [
     "image_command",
     "forks_group",
     "watch_command"
+    "start_proxy"
 ]
