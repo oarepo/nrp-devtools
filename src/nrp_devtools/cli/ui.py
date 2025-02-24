8a6ddce11443c7c897c8f437a6b0ebf7c949486e
Mirek Simek <miroslav.simek@gmail.com>
2025-02-24 07:56:02 +0100

    Scaffold for a detail page

--- src/nrp_devtools/cli/ui.py
+++ src/nrp_devtools/cli/ui.py
@@ -1,14 +1,13 @@
-import json
 from pathlib import Path
+from typing import Any
 
 import click
 
 from ..commands.ui.create import (
-    create_model_ui,
     create_page_ui,
-    register_model_ui,
     register_page_ui,
 )
+from ..commands.ui.create_detail import create_detail_page
 from ..commands.utils import capitalize_name, make_step
 from ..config import OARepoConfig
 from ..config.ui_config import UIConfig
@@ -86,65 +85,37 @@ def model_group():
     """
 
 
-@model_group.command(
-    name="create",
-    help="""Create a new UI for metadata model. 
-    The model-name is the name of the model, such as documents or records,
-    ui-name is the name of the ui (default is the same as model-name).
-    ui-endpoint, if not passed, is taken from the model's resource-config, 
-    field base-html-url.
-    """,
+@model_group.command(name="detail", help="Scaffold detail page for a new model")
+@click.argument("model_name")
+@click.option(
+    "--primitive-type",
+    "-p",
+    type=str,
+    multiple=True,
+    help="Add a mapping for primitive type (type:component)",
 )
-@click.argument("model-name")
-@click.argument("ui-name", required=False)
-@click.argument(
-    "ui-endpoint",
-    required=False,
+@click.option(
+    "--complex-type",
+    "-c",
+    type=str,
+    multiple=True,
+    help="Add a mapping for complex type (type:component)",
 )
-@command_sequence(save=True)
-def create_model(config: OARepoConfig, model_name, ui_name, ui_endpoint, **kwargs):
-    """
-    Create a new UI model.
-    """
-    if not ui_name:
-        ui_name = model_name
-
-    ui_name = ui_name.replace("-", "_")
-
-    if config.get_ui(ui_name, default=None):
-        click.secho(f"UI {ui_name} already exists", fg="red", err=True)
-        return
-
-    def set_ui_configuration(config: OARepoConfig, *args, **kwargs):
-        nonlocal ui_endpoint
-
-        model = config.get_model(model_name)
-
-        model_data = json.loads(
-            (
-                config.repository_dir / model.model_name / "models" / "records.json"
-            ).read_text()
-        )
-        api_service = model_data["model"]["service-config"]["service-id"]
-        ui_serializer_class = model_data["model"]["json-serializer"]["class"]
-        if not ui_endpoint:
-            ui_endpoint = model_data["model"]["resource-config"]["base-html-url"]
-
-        config.add_ui(
-            UIConfig(
-                name=ui_name,
-                endpoint=ui_endpoint,
-                model=model_name,
-                api_service=api_service,
-                ui_serializer_class=ui_serializer_class,
-            )
-        )
-
-    return (
-        set_ui_configuration,
-        make_step(create_model_ui, ui_name=ui_name),
-        make_step(register_model_ui, ui_name=ui_name),
+@command_sequence()
+def create_ui_detail_page(
+    config: OARepoConfig,
+    model_name: str,
+    primitive_type: list[str],
+    complex_type: list[str],
+    **kwargs: Any,
+):
+    create_detail_page(
+        config,
+        model_name,
+        builtin_primitive_types=dict([x.split(":") for x in primitive_type]),
+        builtin_complex_types=dict([x.split(":") for x in complex_type]),
     )
+    return None
 
 
 @ui_group.group(name="components")
